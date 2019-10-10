adcffdb94e27f3f1ad15ae300d03901eea934067
Brian Falasz <brianfalasz@gmail.com>
2019-10-09 21:10:39 -0500

    added a few functions in order to make the buttons in the app functional

--- second_flutter_app/lib/main.dart
+++ second_flutter_app/lib/main.dart
@@ -7,6 +7,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("answer chosen!");
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
@@ -19,9 +23,17 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: Text("My First Real Flutter App")),
         body: Column(children: [
           Text('The question!'),
-          RaisedButton(child: Text('Answer 1'), onPressed: null),
-          RaisedButton(child: Text('Answer 2'), onPressed: null),
-          RaisedButton(child: Text('Answer 3'), onPressed: null),
+          RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: () {
+              print("answer 2 chosen!");
+            },
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: () => print("Answer 3 chosen!"),
+          ),
         ]),
       ),
     );
