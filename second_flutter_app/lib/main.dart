1c0fa80b324c385f40a8696fba4153e88681195e
Brian Falasz <brianfalasz@gmail.com>
2019-10-10 22:55:04 -0500

    learned how to write conditional statements inside of a widget

--- second_flutter_app/lib/main.dart
+++ second_flutter_app/lib/main.dart
@@ -19,39 +19,41 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIdx = 0;
 
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Dog', 'Cat', 'Bird', 'Snake'],
+    },
+    {
+      'questionText': 'What\'s your favorite genre of music?',
+      'answers': ['Pop', 'Rock', 'Rap', 'Folk'],
+    }
+  ];
+
   void _answerQuestion() {
     setState(() {
       _questionIdx = _questionIdx + 1;
     });
-    print(_questionIdx);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Dog', 'Cat', 'Bird', 'Snake'],
-      },
-      {
-        'questionText': 'What\'s your favorite genre of music?',
-        'answers': ['Pop', 'Rock', 'Rap', 'Folk'],
-      }
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("My First Real Flutter App")),
-        body: Column(children: [
-          Question(questions[_questionIdx]['questionText']),
-          ...(questions[_questionIdx]['answers'] as List<String>).map((answer) {
-            return Answer(_answerQuestion, answer);
-          }).toList()
-        ]),
+        body: _questionIdx < questions.length
+            ? Column(children: [
+                Question(questions[_questionIdx]['questionText']),
+                ...(questions[_questionIdx]['answers'] as List<String>)
+                    .map((answer) {
+                  return Answer(_answerQuestion, answer);
+                }).toList()
+              ])
+            : Center(child: Text("You finished the test!")),
       ),
     );
   }
